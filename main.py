726464733a99cb0acd37269317268220a3213ee4
Nay Chi Than Shwe <naychithanshwe94@gmail.com>
2024-01-20 00:39:12 +1100

    screen created using turtle library

---
+++ main.py
@@ -0,0 +1,17 @@
+# pseudocode
+# Step 1: Create a snake
+# Step 2: Make the snake move forward
+# Step 3: Make the snake controlled by arrow keys
+# Step 4: collision detection with food
+# Step 5: Scoreboard
+# step 6: collision detection with wall
+# step 7: collision with tail
+
+from turtle import Turtle,Screen
+
+screen = Screen()
+screen.setup(600,600)
+screen.title("Snake Game")
+screen.bgcolor("Black")
+
+screen.exitonclick()
